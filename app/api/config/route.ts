d5f557fbfa41b47a94ea4ec324e1637facd8861f
yayajjd <153936995+yayajjd@users.noreply.github.com>
2026-06-20 23:35:50 +0200

    Enhance GET response with environment password checks

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -8,13 +8,17 @@ import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
-const SUBSCRIPTION_SOURCES = process.env.SUBSCRIPTION_SOURCES || process.env.NEXT_PUBLIC_SUBSCRIPTION_SOURCES || '';
+const SUBSCRIPTION_SOURCES =
+  process.env.SUBSCRIPTION_SOURCES || process.env.NEXT_PUBLIC_SUBSCRIPTION_SOURCES;
 
 export async function GET() {
-    // 临时打印环境变量
+  // 临时打印环境变量
   console.log("ACCESS_PASSWORD:", process.env.ACCESS_PASSWORD);
   console.log("PERSIST_PASSWORD:", process.env.PERSIST_PASSWORD);
-    return NextResponse.json({
-        subscriptionSources: SUBSCRIPTION_SOURCES,
-    });
+
+  return NextResponse.json({
+    subscriptionSources: SUBSCRIPTION_SOURCES,
+    hasEnvPassword: !!process.env.ACCESS_PASSWORD,
+    persistPassword: !!process.env.PERSIST_PASSWORD,
+  });
 }
